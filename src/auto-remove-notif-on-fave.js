334d71cf2725ef170a1fd24ffe09784c673e29b2
f1r3w4rr10r <entbrannt@gmx.de>
2025-06-08 14:04:40 +0200

    feat: implement auto-remove-notif-on-fave

--- src/auto-remove-notif-on-fave.js
+++ src/auto-remove-notif-on-fave.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto-remove notifications on fave
 // @namespace    https://github.com/f1r3w4rr10r/fa-utils
-// @version      0.0.1
+// @version      1.0.0
 // @description  This automatically removes submission notifications, when faving a submission.
 // @author       f1r3w4rr10r
 // @match        https://www.furaffinity.net/view/*
@@ -12,4 +12,45 @@
 
 (async function () {
   "use strict";
+
+  const favLinks = Array.from(
+    document.querySelectorAll(
+      '.favorite-nav > [href^="/fav/"], .fav > [href^="/fav/"]',
+    ),
+  );
+
+  for (const favLink of favLinks) {
+    if (!(favLink instanceof HTMLAnchorElement))
+      throw new Error("'favLink' was not an instance of 'HTMLAnchorElement'.");
+
+    favLink.addEventListener("click", async (event) => {
+      event.preventDefault();
+
+      const href = favLink.href;
+      const urlMatch = href.match(/\/fav\/(\d+)\//);
+      if (!urlMatch) {
+        console.error("The fav URL did not match.", href);
+        throw new Error("The fav URL did not match.");
+      }
+
+      const submissionId = urlMatch[1];
+      if (!submissionId) throw new Error("Could not extract a submission ID.");
+
+      const result = await fetch("/msg/submissions/old@24/", {
+        method: "POST",
+        body: new URLSearchParams({
+          "messagecenter-action": "remove_checked",
+          "submissions[]": submissionId,
+        }),
+        redirect: "manual",
+      });
+
+      if (result.type !== "opaqueredirect") {
+        console.error("Could not remove the submission notification.", result);
+        throw new Error("Could not remove the submission notification.");
+      }
+
+      window.location.assign(href);
+    });
+  }
 })();
