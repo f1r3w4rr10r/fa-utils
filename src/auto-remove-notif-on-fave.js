52a2745a1277d1ffba61d77108e17032f9b6c0a3
f1r3w4rr10r <entbrannt@gmx.de>
2025-06-08 12:09:27 +0200

    feat: add auto-remove-notif-on-fave

---
+++ src/auto-remove-notif-on-fave.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto-remove notifications on fave
+// @namespace    https://github.com/f1r3w4rr10r/fa-utils
+// @version      0.0.1
+// @description  This automatically removes submission notifications, when faving a submission.
+// @author       f1r3w4rr10r
+// @match        https://www.furaffinity.net/view/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @license      MIT
+// @grant        none
+// ==/UserScript==
+
+(async function () {
+  "use strict";
+})();
